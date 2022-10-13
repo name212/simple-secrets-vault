8dc88d74f2eb19a72980fc5ff53354f7cebaaab9
Nickolay Mitrofanov <nick@local>
2022-10-13 14:25:08 +0300

    ++

---
+++ src/main/kotlin/LoadVault.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class LoadVault {
+    private JComboBox comboBox1;
+    private JPanel panel1;
+    private JPanel localPanel;
+    private JTextField fileInput;
+    private JButton openFileDlg;
+    private JPanel yandex;
+    private JTextField yaFileInput;
+    private JLabel fileLbl;
+    private JButton loadButton;
+
+}
